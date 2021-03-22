fc7d3deca7fa7ca543a40bfe4f42240864ad6762
uo271432 <uo271432@uniovi.es>
2021-03-22 15:26:53 +0100

    SDI-2021-614-7.5-Vistas y motores de plantillas.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let express = require('express');
 let app = express();
 
+let swig = require('swig');
+
 app.use(express.static('public'));
 
 let bodyParser = require('body-parser');
@@ -13,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', 8081);
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
 
 // lanzar el servidor
 app.listen(app.get('port'), function() {
