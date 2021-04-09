90921976dd84cd0b1aac7e17bd2ea4182b3f8b62
uo271432 <uo271432@uniovi.es>
2021-04-09 19:21:30 +0200

    SDI-2021-614-9.1-Gestión de colecciones, eliminar documento.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -195,4 +195,14 @@ function paso2ModificarAudio(files, id, callback){
         callback(true); // FIN
     }
     };
+    app.get('/cancion/eliminar/:id', function (req, res) {
+        let criterio = {"_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.eliminarCancion(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+                res.redirect("/publicaciones");
+            }
+        });
+    });
 };
